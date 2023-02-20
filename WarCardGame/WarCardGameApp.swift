1d5d23596a6924f19ab775ad08a14ce5f7ac55af
Bananaslama277 <tracesmith555@gmail.com>
2023-02-20 08:14:22 -0600

    Initial Commit

---
+++ WarCardGame/WarCardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarCardGameApp.swift
+//  WarCardGame
+//
+//  Created by Trace Smith on 2/20/23.
+//
+
+import SwiftUI
+
+@main
+struct WarCardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
