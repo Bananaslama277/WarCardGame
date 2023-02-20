b33d9ac6961cf2d9ed19670c4bbf781b7b5c129e
Bananaslama277 <tracesmith555@gmail.com>
2023-02-20 08:21:46 -0600

    initial commit

--- WarCardGame/ContentView.swift
+++ WarCardGame/ContentView.swift
@@ -1,21 +1,69 @@
-//
-//  ContentView.swift
-//  WarCardGame
-//
-//  Created by Trace Smith on 2/20/23.
-//
 
 import SwiftUI
 
 struct ContentView: View {
+
+    // State values/variables
+    @State var playerScore = 0
+    @State var cpuScore = 0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Image("background")
+            
+            VStack {
+                Spacer()
+                
+                Image("logo")
+                
+                Spacer()
+                
+                HStack {
+                    Spacer()
+                    Image("card5")
+                    Spacer()
+                    Image("card5")
+                    Spacer()
+                }
+                
+                Spacer()
+                
+                Button {
+                    print("Button clicked!")
+                } label: {
+                    Image("dealbutton")
+                }
+                
+                Spacer()
+                
+                HStack {
+                    Spacer()
+                    VStack {
+                        Text("Player")
+                            .font(.headline)
+                            .foregroundColor(Color.white)
+                            .padding(.bottom, 10.0)
+
+                        Text(String(playerScore))
+                            .font(.largeTitle)
+                            .foregroundColor(Color.white)
+                    }
+                    Spacer()
+                    VStack {
+                        Text("CPU")
+                            .font(.headline)
+                            .foregroundColor(Color.white)
+                            .padding(.bottom, 10.0)
+
+                        Text(String(cpuScore))
+                            .font(.largeTitle)
+                            .foregroundColor(Color.white)
+                    }
+                    Spacer()
+                }
+                Spacer()
+            }
         }
-        .padding()
     }
 }
 
